55fd4fd737662a9c7d667023bcac660a04b5281d
Nacim Osman <nmosman2002@gmail.com>
2024-02-27 19:08:48 -0600

    Created module for Gilded Rose and refactored code to have default class as item and subclasses for each case

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,52 +1,17 @@
-class GildedRose
-  attr_reader :name, :days_remaining, :quality
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
-  end
-
-  def tick
-    case name
-    when "Normal Item"
-      normal_tick
-    when 'Aged Brie'
-      brie_tick
-    when 'Sulfuras, Hand of Ragnaros'
-      sulfuras_tick
-    when "Backstage passes to a TAFKAL80ETC concert"
-      backstage_tick
-    end
-
-  end
-
-  def normal_tick
-    @item = Normal.new(quality, days_remaining)
-    item.tick
-  end
-
-  def brie_tick
-    @item = Brie.new(quality, days_remaining)
-    item.tick
-  end
-
-  def sulfuras_tick
-   @item = Sulfuras.new(quality, days_remaining)
-   item.tick
-  end
-
-  def backstage_tick
-    @item = Backstage.new(quality, days_remaining)
-    item.tick
-  end
-
-  class Normal
-    attr_reader: :quality, :days_remaining
     def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality. days_remaining
+      @quality = quality
+      @days_remaining = days_remaining
     end
 
+    def tick
+    end
+  end
+
+  class Normal < Item
     def tick
       @days_remaining -= 1
       return if quality == 0
@@ -55,11 +20,7 @@ class GildedRose
     end
   end
 
-  class Brie
-    attr_reader: :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
+  class Brie < Item
     def tick
       @days_remaining -= 1
       return if @quality >= 50
@@ -68,20 +29,7 @@ class GildedRose
     end
   end
 
-  class Sulfuras
-    attr_reader:quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-    def tick
-    end
-  end
-
-  class Backstage
-    attr_reader:quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
+  class Backstage < Item
     def tick
       @days_remaining -= 1
       return              if @quality >= 50
@@ -92,3 +40,26 @@ class GildedRose
       @quality += 1 if @days_remaining < 5
     end
   end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -=2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
+
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
+
+end
