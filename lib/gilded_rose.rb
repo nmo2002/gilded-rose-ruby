61731b0fc9a72902c46c106ce610c320ea37a238
Nacim Osman <nmosman2002@gmail.com>
2024-02-27 18:09:57 -0600

    Created classes for each case name and specified functions within them making it simpler

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -22,30 +22,73 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -= 1
-    return if quality == 0
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
 
   def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
+    @item = Brie.new(quality, days_remaining)
+    item.tick
   end
 
   def sulfuras_tick
+   @item = Sulfuras.new(quality, days_remaining)
+   item.tick
   end
 
   def backstage_tick
-    @days_remaining -= 1
-    return              if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
   end
 
-end
+  class Normal
+    attr_reader: :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality. days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if quality == 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie
+    attr_reader: :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+  class Sulfuras
+    attr_reader:quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+    end
+  end
+
+  class Backstage
+    attr_reader:quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      @days_remaining -= 1
+      return              if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
